7dae25f138b2765f24ca9baf554cabde56604e7b
GHNDS <gabriel.private1998@gmail.com>
2022-02-07 17:03:26 -0300

    Correção nos comentários

--- src/model/exceptions/DomainException.java
+++ src/model/exceptions/DomainException.java
@@ -5,8 +5,8 @@ public class DomainException extends RuntimeException{
 	//DomainException é uma classe cujo objetivo é inserir erros personalizados e também
 	//Realizar a boa prática de reaproveitamento de código com uma simples implementação
 	
-	//Utilizado o *** extends RuntimeException *** para ignorar e retirar os trows das validações
-	//nos métodos, normalmente é utilizada somente Exception
+	//Utilizado o *** extends RuntimeException *** para ignorar e retirar 
+	//a obrigação dos trows das validações nos métodos.
 	
 	private static final long serialVersionUID = 1L;
 	
